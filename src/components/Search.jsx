61ec37e52aca4c03f1c2107d2fffaea6a9a7ee54
Jpark <web.jpark@0gmail.com>
2023-11-08 23:30:41 -0700

    added 'trim()' to search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -9,8 +9,10 @@ export default function Search({ onSearch }) {
 	const handleSearchSubmit = (e) => {
 		e.preventDefault();
 
-		if (search.length > 0) onSearch(search);
-		setSearch('');
+		if (search.trim().length > 0) {
+			onSearch(search.trim());
+			setSearch('');
+		}
 	};
 
 	useEffect(() => {
