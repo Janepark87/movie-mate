e6730a7090356b417b319eb1b622d0847017088e
Jpark <web.jpark@gmail.com>
2025-01-21 15:46:58 -0700

    remove useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import useMovies from './hooks/useMovies';
 import useLocalStorageState from './hooks/useLocalStorageState';
 
